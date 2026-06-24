707ce1430e1627013721fa6dde79b9279984f8b4
“Alfi <alfizikri605@gmail.com>
2026-06-24 10:11:39 +0700

    fix final db port type

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Session configuration
 const sessionStore = new MySQLStore({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST || process.env.MYSQL_HOST,
+  user: process.env.DB_USER || process.env.MYSQL_USER,
+  password: process.env.DB_PASSWORD || process.env.MYSQL_PASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQL_DATABASE,
+  port: Number(process.env.DB_PORT || process.env.MYSQL_PORT || 3306),
   createDatabaseTable: true,
   schema: {
     tableName: "express_sessions",
