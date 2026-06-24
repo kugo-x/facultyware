e1d4bfea9ba94f35ac568277acd7d96872290ba3
“Alfi <alfizikri605@gmail.com>
2026-06-24 10:03:33 +0700

    nambah dikit lagi

--- middlewares/error.js
+++ middlewares/error.js
@@ -8,9 +8,8 @@ const notFoundHandler = (req, res, next) => {
 // error handler
 const errorHandler = (err, req, res, next) => {
   console.error("Global Error Handler:", err);
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.message = err.message || String(err);
+  res.locals.error = err; // Force expose error in all environments
 
   // render the error page
   res.status(err.status || 500);
